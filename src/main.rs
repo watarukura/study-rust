0c618232d299d5c138d22fdf119c1473b293e85b
Wataru Kurashima <what.r.j150@gmail.com>
2021-07-21 12:51:24 +0900

    feat: 🎸 use rayon and output line_no

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::read_to_string;
 use structopt::StructOpt;
+use rayon::prelude::*;
 
 #[derive(StructOpt)]
 #[structopt(name = "rsgrep")]
@@ -11,23 +12,19 @@ struct GrepArgs {
 }
 
 fn grep(state: &GrepArgs, content: String, file_name: &str) {
+    let mut line_no = 0;
     for line in content.lines() {
+        line_no = line_no + 1;
         if line.contains(state.pattern.as_str()) {
-            println!("{}: {}", file_name, line);
+            println!("{}({}): {}", file_name, line_no, line);
         }
     }
 }
 
 fn run(state: GrepArgs) {
-    // for file in state.path.iter() {
-    //     match read_to_string(file) {
-    //         Ok(content) => grep(&state, content, file),
-    //         Err(reason) => println!("{}", reason),
-    //     }
-    // }
     state
         .path
-        .iter()
+        .par_iter()
         .for_each(|file| match read_to_string(file) {
             Ok(content) => grep(&state, content, &file),
             Err(reason) => println!("{}", reason),
